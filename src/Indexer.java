c8f39df5f931b896cc047e345c52ea5ad72b53dc
jeniknaum5 <zhenyanaumenko20@gmail.com>
2021-05-17 22:03:31 +0300

    feat: added function buildIndex
    This function implements:
    Reading from data files in directories that were added earlier;
    Getting rid of unnecessary characters in texts;
    Construction of tokens;
    Filling in the dictionary (words, files, where these words are)

--- src/Indexer.java
+++ src/Indexer.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Indexer {
 
@@ -15,8 +20,10 @@ public class Indexer {
     public static void main(String[] args) {
         selectSizeOfData();
         File[] pathToDirections = initFiles();
-        for(File item: pathToDirections)
-            System.out.println(item);
+
+        HashMap<String, ArrayList<String>> dictionary = new HashMap<>();
+
+        buildIndex(pathToDirections, dictionary);
         System.out.println("check");//for debugging
     }
 
@@ -36,4 +43,39 @@ public class Indexer {
                 new File("data//aclImdb//train//unsup")};
     }
 
+    public static void buildIndex(File[] paths, HashMap<String, ArrayList<String>> dictionary) {
+        for (File path : paths) {
+            int numberOfPath = 0;
+            File dir = path;
+
+            if (dir.isDirectory()) {
+                File[] files = dir.listFiles();
+                for (File item : files) {
+
+                    String fileName = item.getName();
+
+                    if (Integer.parseInt(fileName.replaceAll("_+\\d+.txt", "")) >= startIndex[numberOfPath]
+                            && Integer.parseInt(fileName.replaceAll("_+\\d+.txt", "")) < endIndex[numberOfPath]) {
+                        try (BufferedReader bufReader = new BufferedReader(new FileReader(item))) {
+                            String line;
+                            while ((line = bufReader.readLine()) != null) {
+                                line = line.replaceAll("<br /><br />", "");
+
+
+                                String[] words = line.split("\\s*(\\s|,|!|_|\\.)\\s*");
+
+                                for (String word : words) {
+                                    dictionary.computeIfAbsent(word, k -> new ArrayList<String>())
+                                            .add(dir.getParent() + "\\" + dir.getName() + "\\" + path.getName());
+                                }
+                            }
+                        } catch (IOException ex) {
+                            System.out.println(ex.getMessage());
+                        }
+                    }
+                }
+            }
+            numberOfPath++;
+        }
+    }
 }
